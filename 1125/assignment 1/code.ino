71df19e3b51bf16e85b4db9a564fea375cd8826a
XiaoliHan19 <89835320+XiaoliHan19@users.noreply.github.com>
2021-11-25 10:52:15 +0400

    Update code.ino

--- 1125/assignment 1/code.ino
+++ 1125/assignment 1/code.ino
@@ -1,1 +1,16 @@
-
+int sensorValues = A0;
+ 
+void setup() {
+  Serial.begin(9600);
+}
+ 
+void loop() {
+  if (Serial.available()) {
+    if (Serial.read() == '\n') {
+      int sensor = analogRead(sensorValues);
+      delay(1);
+      //print 
+      Serial.println(sensor);
+    }
+  }
+}
