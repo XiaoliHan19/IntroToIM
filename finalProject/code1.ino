f4aa61e75b66687847383f38bb8dfbd824bdb973
XiaoliHan19 <89835320+XiaoliHan19@users.noreply.github.com>
2021-12-12 10:49:22 +0400

    Update code1.ino

--- finalProject/code1.ino
+++ finalProject/code1.ino
@@ -1,4 +1,3 @@
-char val;
 int pushButton1 = 2;
 int pushButton2 = 3;
 int pushButton3 = 4;
@@ -6,32 +5,45 @@ int pushButton4 = 5;
 
 // the setup routine runs once when you press reset:
 void setup() {
-  // initialize serial communication at 9600 bits per second:
   Serial.begin(9600);
   // make the pushbutton's pin an input:
-  pinMode(2, INPUT);
-  pinMode(3, INPUT);
-  pinMode(4, INPUT);
-  pinMode(5, INPUT);
+  pinMode(pushButton1, INPUT);
+  pinMode(pushButton2, INPUT);
+  pinMode(pushButton3, INPUT);
+  pinMode(pushButton4, INPUT);
 }
 
 // the loop routine runs over and over again forever:
 void loop() {
-  // read the input pin:
-  int button1State = digitalRead(2);
-  // print out the state of the button:
-  Serial.println(button1State);
-  delay(500);        // delay in between reads for stability
+  //page 0
+  if (digitalRead(pushButton1) == HIGH) {
+    Serial.write(1);
+  } else {
+    Serial.write(0);
+  }
+  delay(100);
 
-  int button2State = digitalRead(3);
-  Serial.println(button2State);
-  delay(500);
+  //page 1 to 2
+  if (digitalRead(pushButton2) == HIGH) {
+    Serial.write(3);
+  } else {
+    Serial.write(2);
+  }
+  delay(100);
 
-  int button3State = digitalRead(4);
-  Serial.println(button3State);
-  delay(500);
+  //page 1 to 3
+  if (digitalRead(pushButton3) == HIGH) {
+    Serial.write(4);
+  } else {
+    Serial.write(2);
+  }
+  delay(100);
 
-  int button4State = digitalRead(5);
-  Serial.println(button4State);
-  delay(500);
+  //page 1 to 4
+  if (digitalRead(pushButton4) == HIGH) {
+    Serial.write(5);
+  } else {
+    Serial.write(2);
+  }
+  delay(100);
 }
