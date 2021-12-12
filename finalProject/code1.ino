efa8641c8ada5eaf3a453399aa7124fe485e58e8
XiaoliHan19 <89835320+XiaoliHan19@users.noreply.github.com>
2021-12-12 15:04:16 +0400

    Update code1.ino

--- finalProject/code1.ino
+++ finalProject/code1.ino
@@ -2,6 +2,7 @@ int pushButton1 = 2;
 int pushButton2 = 3;
 int pushButton3 = 4;
 int pushButton4 = 5;
+int pushButton5 = 6;
 
 // the setup routine runs once when you press reset:
 void setup() {
@@ -11,6 +12,7 @@ void setup() {
   pinMode(pushButton2, INPUT);
   pinMode(pushButton3, INPUT);
   pinMode(pushButton4, INPUT);
+  pinMode(pushButton5, INPUT);
 }
 
 // the loop routine runs over and over again forever:
@@ -42,6 +44,14 @@ void loop() {
   //page 1 to 4
   if (digitalRead(pushButton4) == HIGH) {
     Serial.write(5);
+  } else {
+    Serial.write(2);
+  }
+  delay(100);
+
+  //jump back to page1
+  if (digitalRead(pushButton5) == HIGH){
+    Serial.write(6);
   } else {
     Serial.write(2);
   }
